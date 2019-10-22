53737aea822e9ae50a31a10f724a8362bf4d69d4
wangbinchang <binchang.wang@xgo.one>
2019-10-22 11:41:04 +0800

    数组方法重写

--- packages/vue/dist/vue2.0Reactive.js
+++ packages/vue/dist/vue2.0Reactive.js
@@ -1,13 +1,41 @@
+const arrayProto = Array.prototype
+const arrayMethods = Object.create(arrayProto)
+
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse'
+]
+methodsToPatch.forEach(method=>{
+  arrayMethods[method] = function(){ 
+    //函数劫持 把函数进行重写 
+    // 而内部实际上继续调用原来的方法但在这里我们可以去调用更新视图的方法
+     console.log('数组 更新啦...')
+      arrayProto[method].call(this,...arguments)
+  }
+});
+
 
 function observer(data){
   // 判断是否为对象 如果不是则直接返回，Object.defineProperty是对象上的属性
   if(typeof data !== 'object' || data == null){
     return data;
   }
-  for(let key in data){
-    defineReactive(data,key,data[key]);
+  if(Array.isArray(data)){ // 如果是数组,则对数据进行遍历并对其value进行递归监听
+    // 在这里对数组方法进行重写 即函数劫持
+    Object.setPrototypeOf(data, arrayMethods); 
+    for(let i = 0; i< data.length ;i++){
+        observer(data[i]);
+    }
+  } else {
+    for(let key in data){
+      defineReactive(data,key,data[key]);
+    }
   }
- 
 }
 function defineReactive(data,key,value){
   observer(value); // 递归 继续对当前value进行拦截
@@ -36,7 +64,7 @@ function reactiveSet (data,key,value) {
 }
 
 
-let data = {name:'',age:18};
+let data = {name:'',attr:[1,2,3,4,5,100]};
 observer(data)
 
 // 给data里面的name赋值 = "Eno"
@@ -44,5 +72,9 @@ observer(data)
 // console.log(data.name);  
 //  data.age = 12
 //  console.log(data.age); 
-reactiveSet(data,'gender','男')
-console.log(data.gender)
+// reactiveSet(data,'gender','男')
+// console.log(data.gender)
+// console.log(data.attr)
+data.attr.push(10000); 
+data.attr.splice(0,1); 
+
